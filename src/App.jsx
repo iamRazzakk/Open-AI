da0515c7a4c65ef4e99a40a84af54e3a3dcd081a
iamRazzakk <rakibt23p@gmail.com>
2024-07-04 22:15:55 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      {/* sidebar */}
+      <div className="sidebar">
+        <div className="uperside">
+
+        </div>
+      </div>
+      {/* main part */}
+      <div className="main">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
